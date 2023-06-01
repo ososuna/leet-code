d77d56ade83991432d7304ba7ad25ee695dcfdb5
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-06-01 13:46:29 -0600

    refactor roman to int

--- src/recursion/roman.js
+++ src/recursion/roman.js
@@ -4,33 +4,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-  var values = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-
-  var sum = 0;
-  var i = 0;
-
-  while(i < s.length) {
-    if (i + 1 < s.length && values[s[i]] < values[s[i+1]]) {
-      // if next 3 are the same, then recursion
-      if (i + 3 < s.length && s[i+1] === s[i+2] && s[i+1] === s[i+3]) {
-        return sum + romanToInt(s[i+1] + s[i+2] + s[i+3]);
-      }
-      sum += (values[s[i+1]] - values[s[i]]);
-      i+=2;
-      continue;
-    }
-    sum += values[s[i]];
-    i++;
+  const sym = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000
   }
-  return sum;
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const cur = sym[s[i]];
+    const next = sym[s[i + 1]];
+
+    if (cur < next) {
+      result += next - cur;
+      i++;
+    } else {
+      result += cur;
+    }
+  }
+
+  return result;
 };
 
 module.exports = romanToInt;
