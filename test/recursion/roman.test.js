3540c0adf11225e6f20e29e1a3b5ffa782a48ffc
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2023-06-01 13:45:05 -0600

    roman to int unit test

---
+++ test/recursion/roman.test.js
@@ -0,0 +1,21 @@
+const romanToInt = require('../../src/recursion/roman');
+
+test('convert III to 3', () => {
+  expect(romanToInt('III')).toBe(3);
+});
+
+test('convert IX to 9', () => {
+  expect(romanToInt('IX')).toBe(9);
+});
+
+test('convert LVIII to 58', () => {
+  expect(romanToInt('LVIII')).toBe(58);
+});
+
+test('convert MCMXCIV to 1994', () => {
+  expect(romanToInt('MCMXCIV')).toBe(1994);
+});
+
+test('convert CDXLIV to 444', () => {
+  expect(romanToInt('CDXLIV')).toBe(444);
+});
